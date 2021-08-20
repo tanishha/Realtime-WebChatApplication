b7d896356dc4c23d922cb0e30632731a9733da0d
Tanisha Chaudhary <tanishach203@gmail.com>
2021-08-21 01:28:49 +0545

    user added

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -1,14 +1,34 @@
-const usermodel =require("./user.model")
+const usermodel = require("./user.model")
 
-async function addUser(req,res,next){
+async function addUser(req, res, next) {
+    try {
+        let user = await usermodel.findOne({
+            googleId: req.body.googleId
+        })
+        if (user) {
+            return res.status(200).json("User already exists")
+        }
+        const newUser = new usermodel(req.body)
+        await newUser.save()
+            .then(function (data) {
+                res.json(data);
+                console.log("Successfully Registered");
+            })
+            .catch(function (err) {
+                next(err);
+                console.log("error", err);
+            });
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+async function getUsers(req, res, next) {
 
 }
 
-async function getUsers(req,res,next){
-    
-}
-
-module.exports={
+module.exports = {
     addUser,
     getUsers
 }
