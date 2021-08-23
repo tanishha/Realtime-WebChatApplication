c656fd3a00764b84eceea8d80173dd3744518433
Tanisha Chaudhary <tanishach203@gmail.com>
2021-08-24 00:39:31 +0545

    user context

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import MainComponent from "./Components/Main/main.component";
 import { AccountProvider } from "./Context/AccountProvider";
-import {TemplateProvider} from "./Context/TemplateProvider";
+import { TemplateProvider } from "./Context/TemplateProvider";
+import UserProvider from "./Context/UserProvider";
 
 function App() {
   return (
     <div>
       <TemplateProvider>
-        <AccountProvider>
-          <MainComponent />
-        </AccountProvider>
+        <UserProvider>
+          <AccountProvider>
+            <MainComponent />
+          </AccountProvider>
+        </UserProvider>
       </TemplateProvider>
     </div>
   );
