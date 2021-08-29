1d4e5bc0ef0183b057e466f4e93a0c0d95cdf9b7
Tanisha Chaudhary <tanishach203@gmail.com>
2021-08-29 10:39:27 +0545

    disconnection

--- socket.js
+++ socket.js
@@ -21,7 +21,9 @@ module.exports = (server) => {
         console.log(users);
         return users.find(user => user.userId === userId);
     }
-
+    const removeUser = (socketId) => {
+        users = users.filter(user => user.socketId !== socketId);
+    }
     io.on("connection", (client) => { //function is called for each new user and dedicated connection is created for each user
         console.log("Socket is active to be connected")
         //connect
@@ -35,11 +37,17 @@ module.exports = (server) => {
             receiverId,
             text
         }) => {
-            const user = getUsers(receiverId); 
+            const user = getUsers(receiverId);
             io.to(user.socketId).emit('getMessage', {
                 senderId,
                 text
             })
+        })
+        //disconnect
+        client.on('disconnect', () => {
+            console.log('user disconnected');
+            removeUser(socket.id);
+            io.emit('getUsers', users);
         })
     })
 }
